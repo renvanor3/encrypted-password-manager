f6af6f7a0bb4b7ead6ce7f6f67cf821ac8c70d28
ali sait <ali-sait.bardak@etu.univ-amu.fr>
2026-05-23 15:26:06 +0200

    fonctions pour chiffrée et déchiffrée des données

--- pwd_manager.py
+++ pwd_manager.py
@@ -1,6 +1,7 @@
 import os
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.cophers.aead import AESGCM
 
 def derivate_key(password, salt):
     kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=salt, iterations=500000)
@@ -9,3 +10,16 @@ def derivate_key(password, salt):
 salt = os.urandom(16)
 key = derivate_key("password", salt)
 print(key.hex())
+
+def cipher(key, text):
+    aesgcm = AESGCM(key)
+    nonce = os.urandom(12)
+    cipher = aesgcm.encryptor(nonce, text.encode(), None)
+    return nonce + cipher
+
+def decipher(key, data):
+    aesgcm = AESGCM(key)
+    nonce = data[:12]
+    cipher = data[12:]
+    decrypted_data = aesgcm.decrypt(nonce, cipher, None)
+    return decrypted_data.decode()
